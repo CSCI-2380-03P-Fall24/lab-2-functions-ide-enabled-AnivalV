0d6b8b7dc65c23de59ac3644320b13f9a159b423
AnivalV <anival.vasquez01@utrgv.edu>
2025-09-14 06:58:44 +0000

    done.

--- FunctionsLab.cpp
+++ FunctionsLab.cpp
@@ -10,18 +10,32 @@ using namespace std; // making life easier so that we do not need to use std::ci
 // Note that I don't want the smallest value
 
 
-int locateSmallest(int array[], int start, int end){
-	return -1; // fix this!
+int locateSmallest(int array[], int start, int end) {
+	int first = array[0];
+	int index = start;
+	for (int i = start; i < end; i++) {
+		if (first > array[i]) {
+			first = array[i];
+			index = i;
+		}
+	}
+	cout << index << endl;
+	return index;
 }
-
 //Write the function called replaceVariable in the given interval such that 
 // output : you decide what is the output (here I put "void" - change this if you need to!)
 // input : an array of int, the target value we are searching for, 
 //     an int which is the starting index of search,
 //     an int which is the ending point of searchspace
 // function: Search in the array from given start to end index and replace the given target value with 1000
-void replaceVariable() {
-	return;
+void replaceVariable(int array[], int start, int end, int value) {
+	for (int i = start; i < end; i++) {
+		if (array[i] == value) {
+			array[i] = 1000;
+		}
+		cout << array[i] << " ";
+	}
+	cout << endl;
 }
 
 
@@ -29,8 +43,11 @@ void replaceVariable() {
 //write a function called printArray to print out the elements of the given array
 //output: nothing
 //input: the array of integers and its size
-void printArray() {
-	return;
+void printArray(int size, int array[]) {
+	for (int i = 0; i < size; i++) {
+		cout << array[i] << " ";
+	}
+	cout << endl;
 }
 
 
@@ -47,7 +64,13 @@ void printArray() {
 //Hint: % is used for modulo operation. 
 //     n%m gives you the remainder after dividing n to m. for example 7%3 = 1 because 7 = 3*2 + 1, then 1 is the remainder.
 
-
-int sumOdds(int array[], int start, int end){
-	return -1;
+int sumOdds(int array[], int start, int end) {
+	int total = 0;
+	for (int i = start; i < end; i++) {
+		if ((array[i] % 2) == 1) {
+			total += array[i];
+		}
+	}
+	cout << total;
+	return total;
 }
